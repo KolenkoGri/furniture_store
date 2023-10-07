608ba2fae2a63d9f23b0c7b7a910e86e32d9edfb
KolenkoGri <kolenkogrigorenko@gmail.com>
2023-10-07 21:28:22 +0400

    add style

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
-import "normalize.css";
-import "./style.scss";
-import { Navigation, Thumbs } from "swiper/modules"
+import 'normalize.css';
+import './style.scss';
+
 import Swiper from 'swiper';
-import "swiper/css";
+import { Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
 
 const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
+  loop: true,
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: true,
   watchSlidesProgress: true,
 });
-
 new Swiper(".product__slider-main", {
+  loop: true,
   spaceBetween: 10,
   navigation: {
-    nextEl: ".product__arrow_next",
-    prevEl: ".product__arrow_prev",
+    nextEl: ".product__arrow-next",
+    prevEl: ".product__arrow-prev",
   },
-  modules: [ Navigation, Thumbs ],
+  modules: [Navigation, Thumbs],
   thumbs: {
     swiper: swiperThumbnails,
   },
 });
+
+
