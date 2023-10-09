8029052757f0e8aba09d8da188f9dabf0792caa9
KolenkoGri <kolenkogrigorenko@gmail.com>
2023-10-09 12:39:39 +0400

    complete style

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-import 'normalize.css';
-import './style.scss';
+import "normalize.css";
+import "./style.scss";
 
-import Swiper from 'swiper';
-import { Navigation, Thumbs } from 'swiper/modules';
-import 'swiper/css';
+import Swiper from "swiper";
+import { Navigation, Thumbs } from "swiper/modules";
+import "swiper/css";
 
 const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
 });
 new Swiper(".product__slider-main", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".product__arrow-next",
+        prevEl: ".product__arrow-prev",
+    },
+    modules: [Navigation, Thumbs],
+    thumbs: {
+        swiper: swiperThumbnails,
+    },
 });
-
-
